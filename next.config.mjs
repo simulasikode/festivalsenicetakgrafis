7cf6992fb2f4883a88c8a274ed2d50dd84b8c1dc
simulasikode <simulasikode@gmail.com>
2024-11-11 12:51:57 +0700

    Update: Index page

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  sassOptions: {
+    silenceDeprecations: ["legacy-js-api"],
+  },
+};
 
 export default nextConfig;
