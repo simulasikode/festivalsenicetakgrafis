936c04d0a060842ae662e9626abe8f7e347a89db
simulasikode <simulasikode@gmail.com>
2024-12-08 18:29:21 +0700

    Update: Conduct

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,7 @@ import remarkGfm from "remark-gfm";
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+  output: "export",
   sassOptions: {
     silenceDeprecations: ["legacy-js-api"],
   },
