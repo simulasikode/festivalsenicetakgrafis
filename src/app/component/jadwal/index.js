76040f3d6b46773c246b1a879f8c08ffb300edb4
simulasikode <simulasikode@gmail.com>
2024-12-15 16:06:39 +0700

    Update: Docs

---
+++ src/app/component/jadwal/index.js
@@ -0,0 +1,10 @@
+import styles from "./jadwal.module.scss";
+
+export default function Jadwal() {
+  return (
+    <div className={styles.jadwal}>
+      <h1>Jadwal Festival</h1>
+      <div>Program Publik</div>
+    </div>
+  );
+}
